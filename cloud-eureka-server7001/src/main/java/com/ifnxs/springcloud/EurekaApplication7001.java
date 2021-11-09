145293f6c6f08d94d92e81204026b722ced0916b
wangqh <wangqh@qq.com>
2021-11-09 14:49:16 +0800

    p15-p18

--- cloud-eureka-server7001/src/main/java/com/ifnxs/springcloud/EurekaApplication7001.java
+++ cloud-eureka-server7001/src/main/java/com/ifnxs/springcloud/EurekaApplication7001.java
@@ -2,12 +2,12 @@ package com.ifnxs.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderApplication81 {
+@EnableEurekaServer
+public class EurekaApplication7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication81.class, args);
+        SpringApplication.run(EurekaApplication7001.class, args);
     }
 }
