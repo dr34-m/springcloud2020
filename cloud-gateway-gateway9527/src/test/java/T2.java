c5910613fc19a00a8a3ddbe9d58dffce65aebd6f
wangqh <wangqh@qq.com>
2021-11-19 15:26:50 +0800

    p71-p73

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+        // 2021-11-19T14:56:45.060+08:00[Asia/Shanghai]
+    }
+}
