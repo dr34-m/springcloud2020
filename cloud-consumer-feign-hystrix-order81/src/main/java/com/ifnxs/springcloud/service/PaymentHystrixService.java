1b871ea40f3b9d52dcdaed3e20e21b8e2a4dbfde
wangqh <wangqh@qq.com>
2021-11-19 10:13:53 +0800

    p56-p64

--- cloud-consumer-feign-hystrix-order81/src/main/java/com/ifnxs/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order81/src/main/java/com/ifnxs/springcloud/service/PaymentHystrixService.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT", fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService {
-
 
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id);
